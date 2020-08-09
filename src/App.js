151aff6259f98864d6da1c0ac401e047075c1c3f
rudylpzjme <rudylpzjme@gmail.com>
2020-08-09 17:28:44 -0500

    Refactor Actions, Redirecting to home after login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import LoginPage from './pages/login-page/login-page.component';
@@ -46,17 +46,21 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/login' component={LoginPage} />
+          <Route exact path='/login' render={ () => this.props.currentUser ? (<Redirect to='/' />) : (<LoginPage />) } />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps)(App);
